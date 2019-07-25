ae5fde269212b7ea4b414e1ee6015923445c13d4
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2019-07-26 01:05:03 +0200

    Add Index

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import BeatLoader from "./BeatLoader"
+import BounceLoader from "./BounceLoader"
+import ClipLoader from "./ClipLoader"
+import DotLoader from "./DotLoader"
+import FadeLoader from "./FadeLoader"
+import GridLoader from "./GridLoader"
+import MoonLoader from "./MoonLoader"
+import PacmanLoader from "./PacmanLoader"
+import PulseLoader from "./PulseLoader"
+import RingLoader from "./RingLoader"
+import RiseLoader from "./RiseLoader"
+import RotateLoader from "./RotateLoader"
+import ScaleLoader from "./ScaleLoader"
+import SkewLoader from "./SkewLoader"
+import SquareLoader from "./SquareLoader"
+import SyncLoader from "./SyncLoader"
+
+export {
+  BeatLoader,
+  BounceLoader,
+  ClipLoader,
+  DotLoader,
+  FadeLoader,
+  GridLoader,
+  MoonLoader,
+  PacmanLoader,
+  PulseLoader,
+  RingLoader,
+  RiseLoader,
+  RotateLoader,
+  ScaleLoader,
+  SkewLoader,
+  SquareLoader,
+  SyncLoader
+}
